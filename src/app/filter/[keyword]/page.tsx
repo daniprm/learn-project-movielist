b689ef9f60afd038b54b9d932bd60077c266f654
daniprm <daniprmnn@gmail.com>
2025-02-24 08:57:52 +0700

    fix streaming

--- src/app/filter/[keyword]/page.tsx
+++ src/app/filter/[keyword]/page.tsx
@@ -1,8 +1,15 @@
 import AppShell from "@/components/layouts/AppShell/AppShell";
 import { Suspense } from "react";
 import Header from "@/components/layouts/Header/Header";
-import MovieSkeleton from "@/components/Skeleton/MovieSkeleton";
-import MoviesWithStreaming from "@/components/Movies/MoviesWithStreaming";
+import Loading from "@/components/Loading/Loading";
+import { getData } from "@/Utilities/Movies/getData";
+import MovieList from "@/components/Movies/MovieList";
+
+const MovieContent = async ({ keyword }: { keyword: string }) => {
+  const query = `http://localhost:5000/movies?${keyword}`;
+  const movies = await getData(query);
+  return <MovieList movies={movies} />;
+};
 
 const Filter = async ({ params }: { params: { keyword: string } }) => {
   const { keyword } = await params;
@@ -11,8 +18,8 @@ const Filter = async ({ params }: { params: { keyword: string } }) => {
   return (
     <AppShell>
       <Header>Hasil Filter</Header>
-      <Suspense fallback={<MovieSkeleton />}>
-        <MoviesWithStreaming keyword={decodedKeyword} />
+      <Suspense fallback={<Loading />}>
+        <MovieContent keyword={decodedKeyword} />
       </Suspense>
     </AppShell>
   );
--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,20 +1,26 @@
 import AppShell from "@/components/layouts/AppShell/AppShell";
 import { Suspense } from "react";
 import MovieDetailPage from "@/components/Movies/Detail/MovieDetailPage";
-import { getMovie } from "@/app/movie/getMovie";
 import Loading from "@/components/Loading/Loading";
-const MovieContent = async ({ movieId }: { movieId: string }) => {
-  const movie = await getMovie(movieId);
-  return <MovieDetailPage movie={movie} />;
+import { getData } from "@/Utilities/Movies/getData";
+import CommentBox from "@/components/Comment/CommentBox";
+const CommentsContent = async ({ movieId }: { movieId: string }) => {
+  const query = `http://localhost:5000/comments?movieId=${movieId}`;
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const commentsData = await getData(query);
+  return <CommentBox movieId={movieId} commentsData={commentsData} />;
 };
 
 const MovieDetail = async ({ params }: { params: { movieId: string } }) => {
   const { movieId } = await params;
+  const query = `http://localhost:5000/movies/${movieId}`;
+  const movie = await getData(query);
 
   return (
     <AppShell>
+      <MovieDetailPage movie={movie} />
       <Suspense fallback={Loading()}>
-        <MovieContent movieId={movieId} />
+        <CommentsContent movieId={movieId} />
       </Suspense>
     </AppShell>
   );
